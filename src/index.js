8f8b89c231b1d1ceaf1cfa7b26b270ecaed02401
katusha4leon <katusha4leon@gmail.com>
2023-10-09 20:46:18 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import user from './components/data/user.json'
-import data from './components/data/data.json'
-import friends from'./components/data/friends.json'
-import transactions from './components/data/transactions.json'
+import user from './data/user.json'
+import data from './data/data.json'
+import friends from'./data/friends.json'
+import transactions from './data/transactions.json'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
